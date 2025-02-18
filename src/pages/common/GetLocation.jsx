628c8fed44c568ea70a5b86e09ccae5530274951
naveenfutureprofilez <naveen@internetbusinesssolutionsindia.com>
2025-02-18 18:33:43 +0530

    mrge changes

--- src/pages/common/GetLocation.jsx
+++ src/pages/common/GetLocation.jsx
@@ -1,3 +1,66 @@
+// import axios from "axios";
+// import React, { useEffect, useRef, useState } from "react";
+
+// export default function GetLocation({ index, onchange, placeholder }) {
+//   const googlemap = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+//   const inputRef = useRef(null);
+//   const [inputText, setInput] = useState(""); // Initialize with an empty string
+//   const [isSuggestionSelected, setIsSuggestionSelected] = useState(false); // Track if a suggestion was selected
+
+//   const [suggestions, setSuggestions] = useState([]);
+//   const getSuggestions = (place) => { 
+//     const resp = axios.get(`https://photon.komoot.io/api/?q=${place}`);
+//     resp.then((re)=>{
+//       console.log("res",re.data.features);
+//       setSuggestions(re.data.features || []);
+//     }).catch((err)=>{
+//       console.log("err",err)
+//     })
+//   }
+
+//   const handleInput = (e) => {
+//     setInput(e.target.value);
+//     setIsSuggestionSelected(false);
+//     onchange && onchange(index, inputText);
+//     getSuggestions(e.target.value);
+//   };
+
+//   // const handleBlur = () => {
+//   //   // When the input loses focus, update the address if no suggestion was selected
+//   // };
+//   const selectAddress = (e) => { 
+//     onchange && onchange(index, e);
+//     setSuggestions(null);
+//   }
+
+//   return (
+//     <div className="relative">
+//       <input
+//         ref={inputRef}
+//         type="text"
+//         name="location"
+//         value={inputText}
+//         onChange={handleInput}
+//         // onBlur={handleBlur}
+//         className="input-sm"
+//         placeholder={placeholder || "Search Location"}
+//       />
+//       {suggestions ? <div className="suggestions_lists bg-white p-2 rounded-xl">
+//           {suggestions && suggestions.map((s,i)=>{
+//             const place = s.properties || null;
+//             return <>
+//               <div  onClick={()=>selectAddress(`${place?.name} ${place?.locality} ${place?.street} ${place?.locality} ${place?.state} ${place?.postcode}`)} className="px-2 py-2 rounded-xl bg-gray-200 text-sm my-1 cursor-pointer">{place?.name} {place?.locality} {place?.street} {place?.locality} {place?.state} {place?.postcode}</div>
+//             </>
+//           })}
+//       </div> : ''}
+//     </div>
+//   );
+// }
+
+
+
+
+
 import React, { useEffect, useRef, useState } from "react";
 
 export default function GetLocation({ index, onchange, placeholder }) {
