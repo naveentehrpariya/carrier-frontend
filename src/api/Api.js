f1eb78351f09a6dab503ba6ab44035d7471c6f17
Naveen Tehrpariya <naveentehrpariya@MacBook-Air-2.local>
2025-07-03 09:24:33 +0530

    merge changes

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-const APP_URL = "https://carrier-backend-drab.vercel.app/";
-// const APP_URL = "http://localhost:8080";
-
+// const APP_URL = process.env.APP_URL || "https://carrier-backend-drab.vercel.app/";
+const APP_URL = "https://13.232.96.215/api";
 const host = window.location.host;
 function getToken(){
   const data = localStorage && localStorage.getItem('token');
